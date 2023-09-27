bb949350ac2407f10b3fbbf20eaf0f6c71cbb7f1
Adriana Chavez <adrifelcha@gmail.com>
2023-09-27 16:21:46 -0700

    Small changes, just testing the function, irrelevant commit

--- code/main.R
+++ code/main.R
@@ -4,26 +4,24 @@
 test <- FALSE
 source("./samplers.R")
 
-samples <- readRDS(file="../posterior-test-eta-omega-cddm.RDS")
+samples <- readRDS(file="../data/posterior-test-eta-omega-cddm.RDS")
 posterior.list <- samples$BUGSoutput$sims.list  # Isolate posteriors
 
-speed_id = 1
-difficulty_id = 1
-deflection_id = 1
-specific.sub = NA
+data <- read.csv("../data/orientation.csv")
+
+trial_type <- list("speed_id" = 1,
+                   "difficulty_id" = 1,
+                   "deflection_id" = 1)
 nSamples = 5000
-getSamples <- function(posterior.list,  # samples$BUGSoutput$sims.list
-                       nSamples,        # 
-                       speed_id,        # 1,2
-                       difficulty_id,   # 1,2,3
-                       deflection_id,   # 1, 2, ..., 7
-                       specific.sub = NA # Choose a specific subject
-                       ){
-  
-  #Identify the cue deflection used here
-  cues_available = c(-70,-50,-20,0,20,50,70)
-  this.cue = cues_available[deflection_id]
-  
+specific.sub = NA
+samplesPerTrialType <- function(nSamples, 
+                                posterior.list,  # samples$BUGSoutput$sims.list
+                                trial_type,
+                                specific.sub = NA, # Choose a specific subject
+                                ){
+  speed_id = trial_type$speed_id
+  difficulty_id = trial_type$difficulty_id
+                     
   # Determine whether a single subject vs all subjects will be examined
   if(is.na(specific.sub)){ 
         sub = 1:ncol(posterior.list$delta)
@@ -46,20 +44,22 @@ getSamples <- function(posterior.list,  # samples$BUGSoutput$sims.list
   beta = post.cue.var[random.samples,]
   var = post.true.var[random.samples,]
   
+  # Identify the cue deflection value
+  cues_available = c(-70,-50,-20,0,20,50,70)
+  this.cue = cues_available[deflection_id]
   # We don't have posterior samples for theta, so we'll sample them
   empty.matrix = matrix(NA,nrow=nSamples,ncol=length(sub))
   z = empty.matrix
   theta.true = empty.matrix
   theta.cue = empty.matrix
   for(i in sub){   # For each subject...
-    # Get an indicator value
-    z[,i] = rbinom(nSamples,1,omega[,i])  
-    # theta centered at true
-    theta.true[,i] = rnorm(nSamples,0,sqrt(var[,i])) 
-    # Transform back to precision so we can insert beta directly
-    tau = (1/var[,i])*beta[,i]  
-    # theta centered at the cue
-    theta.cue[,i] = rnorm(nSamples,this.cue,sqrt(1/tau))
+      # Sample an indicator value per omega sampled
+      z[,i] = rbinom(nSamples,1,omega[,i])  
+      # drift angles centered at true
+      theta.true[,i] = rnorm(nSamples,0,sqrt(var[,i])) 
+      # drift angles centered at cue
+      tau = (1/var[,i])*beta[,i]   # Beta is a scale on the precision, so we transform it
+      theta.cue[,i] = rnorm(nSamples,this.cue,sqrt(1/tau))
   }
   # Transform back from degrees to radians
   theta.cue = degToRad(theta.cue)
@@ -68,26 +68,31 @@ getSamples <- function(posterior.list,  # samples$BUGSoutput$sims.list
   theta = (theta.cue*z)+theta.true
   
   TOTAL = length(sub)*nSamples
-  seed = 1
+  count = 1
   getSamples = array(NA,dim=c(nSamples,3,length(sub)))
   for(i in sub){
     for(j in 1:nSamples){
+        seed <- count
         par = list("drift" = delta[j,i],
                    "theta" = theta[j,i],
                    "tzero" = t0[j,i],
                    "boundary" = eta[j,i])
         x = c(NA,NA)
         while(is.na(x[1])){
-          x = sample.MCMC.cddm(n=1, par, max.RT = 20, seed = seed)
+          x = sample.MCMC.cddm(n=1, par, max.RT = 2.5, seed = seed)
           seed = seed+1
         }
         getSamples[j,1:2,i] = x
         getSamples[j,3,i] = seed
-        cat("Run", seed, "of ", TOTAL, "\n")
-        seed = seed+1
+        cat("Run", count, "of ", TOTAL, "\n")
+        count = count +1
     }
   }
+  
+  
+  return(getSamples)
 }
+
 
   
 
