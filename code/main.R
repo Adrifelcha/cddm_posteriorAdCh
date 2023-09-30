ccc9e3db8c39a64931e590474f3f37f6223f3332
Adrifelcha <adrifelcha@gmail.com>
2023-09-30 13:16:02 -0700

    Ignore commit: Minimal changes

--- code/main.R
+++ code/main.R
@@ -12,6 +12,7 @@ posterior.list <- samples$BUGSoutput$sims.list  # Isolate posteriors
 # Load data set
 rawData <- read.csv("../data/orientation.csv")
 data <- cleanData(rawData)  # Keep relevant columns
+data <- orderData(data)
 
 # General settings
 nPosteriorSamples = 1500
@@ -27,10 +28,5 @@ counts <- table(data[keep,]$sub)
 nPosteriorPredictions <- rep(0,6)
 nPosteriorPredictions[as.numeric(names(counts))] <- as.numeric(counts)
 
-x <- samplesPerTrialType(nPosteriorSamples,     # No. of values sampled from posterior
-                        nPosteriorPredictions, # No. of predicted data points to sample
-                        posterior.list,    # samples$BUGSoutput$sims.list
-                        trial_type,        # List(speed_id, difficulty_id, cue_id)
-                        specific.sub = NA, # Specify what subjects to include
-                        max.RT = max.RT,        # Maximum RT in dataset
-                        track = TRUE)
+x <- samplesPerTrialType(nPosteriorSamples, nPosteriorPredictions, posterior.list,
+                         trial_type, specific.sub = NA, max.RT = max.RT, track = TRUE)
