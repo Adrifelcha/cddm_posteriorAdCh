02a6bc511c10c60b7e3c4c7f02cd5637d631107b
Adriana Chavez <adrifelcha@gmail.com>
2023-10-03 13:28:16 -0700

    Incorporate an example plot contrasting the posterior predictions against the data

--- code/main.R
+++ code/main.R
@@ -19,3 +19,21 @@ data <- orderData(data.0, show.missing=TRUE)
 nPosteriorSamples = 1000
 
 out <- posterior_predictions(data, posterior.list, nPosteriorSamples)
+
+
+
+##################
+# Plot example
+# Trial type 1
+trial_type = list("speed_id" = 1, "difficulty_id" = 2, "cue_id" = 4)
+locate <- locate_trials(data,trial_type, sub=6)
+obs.choice <- data[locate,]$choice %% (2*pi)
+obs <- cbind(obs.choice, data[locate,]$rt)
+
+plot(obs, pch=4, col="red", xlab="Choice", ylab="RT",
+     xlim=c(0,2*pi),ylim=c(0,max(data$rt)))
+for(a in 1:dim(out)[3]){
+    pred <- cbind(out[locate,"choice",a],out[locate,"RT",a])
+    points(pred,col=rgb(0.6,0.5,0.1,0.09))
+}
+
